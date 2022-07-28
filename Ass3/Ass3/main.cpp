983fe2ceee5b8102eb3855a734c8156e8be66d90
PaperGroceries <106277251+PaperGroceries@users.noreply.github.com>
2022-07-27 20:01:36 -0700

    start

---
+++ Ass3/Ass3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Ass3
+//
+//  Created by Aim Kim on 2022-07-27.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
