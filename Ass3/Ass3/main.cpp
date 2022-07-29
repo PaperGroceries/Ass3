31abd29bafd8b37d4e828bd7c03b19f570f00624
PaperGroceries <106277251+PaperGroceries@users.noreply.github.com>
2022-07-28 21:26:46 -0700

    overloaded sum and =

--- Ass3/Ass3/main.cpp
+++ Ass3/Ass3/main.cpp
@@ -12,7 +12,8 @@ using namespace std;
 class Polynomial
 {
     double *arr;
-    double *sum;
+//    double *sum;
+    
     int del_size;
 public:
     Polynomial();
@@ -21,7 +22,7 @@ public:
 //    Polynomial(double, double, double );
 //    Polynomial(double, double, double, double );
     Polynomial(const Polynomial&);
-    double operator+(const Polynomial &);
+    friend Polynomial operator+(const Polynomial &,const Polynomial &);
     void operator*(const Polynomial &);
     void operator=(const Polynomial &);
     void set(int exponent, double coefficient);
@@ -30,15 +31,15 @@ public:
 };
 Polynomial::Polynomial()
 {
-    arr = new double[3];
+    arr = new double[10];
     arr[0] = 1;
     arr[1] = 2;
     arr[2] = 1;
 }
 Polynomial :: Polynomial(int exponent)
 {
-    del_size = exponent + 1;
-    arr = new double[exponent + 1];
+    del_size = exponent +1;
+    arr = new double[exponent +1];
     for (int i = 0; i < (exponent + 1); i++)
     {
         arr[i] = 0;
@@ -47,26 +48,55 @@ Polynomial :: Polynomial(int exponent)
 Polynomial::Polynomial(const Polynomial&y)
 {
     //create dynamic variable
-    //make a deep copy done!
-    arr = new double[10];
-    *arr = *(y.arr);
-}
-double Polynomial::operator+(const Polynomial & RightSide)
-{
-    sum = new double[10];
-    for(int i = 0; i < 10; i++)
+    //make a deep copy
+    arr = new double[del_size];
+//    *arr = *(y.arr);
+    
+    for (int i = 0; i < del_size; i++)
     {
-        sum[i] = arr[i] + RightSide.arr[i];
+        arr[i] = y.arr[i];
     }
-    return *sum;
+    
 }
+ Polynomial operator+(const Polynomial & LeftSide,const Polynomial & RightSide)
+{
+    Polynomial sum;
+   // int n_coefficient;
+    if(LeftSide.del_size >= RightSide.del_size)
+    {
+        sum.del_size = LeftSide.del_size;
+    }
+//    sum = new double[10];
+    else if (RightSide.del_size > LeftSide.del_size)
+    {
+        sum.del_size = RightSide.del_size;
+    }
+     for (int i = 0; i < sum.del_size ; i++)
+     {
+         sum.arr[i] = LeftSide.arr[i] + RightSide.arr[i];
+     }
+     
+//    for(int i = 0; i < 10; i++)
+//    {
+//        sum[i] = arr[i] + RightSide.arr[i];
+//    }
+    return sum;
+    }
 void Polynomial::operator*(const Polynomial & RightSide)
 {
+    Polynomial product;
+//    for (int i = 0; i < sum.del_size ; i++)
+//    {
+//
+//    }
     //double for loop to make mupltiplication and then sum the products
 }
 void Polynomial::operator=(const Polynomial & RightSide)
 {
-    
+    for( int i = 0; i < RightSide.del_size; i++)
+    {
+        arr[i] = RightSide.arr[i];
+    }
 }
 Polynomial::~Polynomial()
 {
@@ -82,19 +112,46 @@ void Polynomial::set(int exponent, double coefficient)
 }
 void Polynomial::get()
 {
-    cout << arr[0];
+//    cout << arr[0];
+    for (int i = 5 - 1; i >= 0; i--)
+    {
+        if (i == 0 && !(arr[i] == 0))
+        {
+            cout << arr[i] << "x" << endl;
+        }
+        else if (!(arr[i] == 0))
+        {
+            cout << arr[i] << "x^" << i << " ";
+        }
+
+//        for (int k + 0; k < i; k++)
+//        {
+//            cout << "^"
+//        }
+    }
     //make actual get with x and  make it empty if index is 0
 }
 
-int main(int argc, const char * argv[]) {
+int main() {
     // i need to find a way to transfer the size of the array
-    std::cout << "Hello, World!\n";
     Polynomial a(4);
+    Polynomial b(4);
+    Polynomial c;
     a.set(1,2);
     a.set(0,1);
     a.set(2,2);
-    a.set(3,3);
+    a.set(3,0);
     a.set(4,5);
+    b.set(1,2);
+    b.set(0,1);
+    b.set(2,4);
+    b.set(3,2);
+    b.set(4,6);
     a.get();
+    cout  << "+" <<endl;
+    b.get();
+    c = (a + b);
+    c.get();
+    
     return 0;
 }
